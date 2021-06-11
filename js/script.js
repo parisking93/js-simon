993c42000ffdcc4c3f830f82d920eeb8594955ef
emmanuelparisi <emmanuel.p2606@gmail.com>
2021-06-11 14:31:07 +0200

    add numeri random + timer

--- js/script.js
+++ js/script.js
@@ -4,4 +4,32 @@
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
 // Un alert() espone 5 numeri generati casualmente.
-console.log('ciao');
+var arr = [];
+for (var i = 0; i < 5;i++) {
+    arr.push(' ' + (Math.floor(Math.random() * 5) + 1) + ' ')
+}
+
+// Da li parte un timer di 30 secondi.
+alert('questi sono i 5 numeri da ricordare: ' + arr);
+var secondi = 5;
+var timer = setInterval(function(){
+    console.log(--secondi + 1);
+    if (!secondi) {
+        clearInterval(timer);
+        // Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+
+    }
+},1000);
+
+
+
+
+
+
+
+
+
+
+
+
+
