f0935af3a39a508b1689be9dec01593691063127
emmanuelparisi <emmanuel.p2606@gmail.com>
2021-06-13 10:35:41 +0200

    add controlli input e messaggio di fine gioco countdown visibile

--- js/script.js
+++ js/script.js
@@ -7,38 +7,57 @@
 var arr = numRandUni(5,1,100);
 // Da li parte un timer di 30 secondi.
 alert('questi sono i 5 numeri da ricordare: ' + arr);
-var secondi = 5;
+var secondi = 3;
+var stopGame = false; 
 var timer = setInterval(function(){
     if (!secondi) {
+        document.getElementById('countdown').innerHTML = "";
         clearInterval(timer);
         // Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
-
         var count = 0;
         var arrRisposte = [];
         while(count < 5) {
-            var numeroUtente = parseInt(prompt('dammi il ' + (count + 1) + '°' + ' numero '));
-            if (!arrRisposte.includes(numeroUtente)) {
-                arrRisposte.push(numeroUtente);
-                count++;
+            var numeroUtente = prompt('dammi il ' + (count + 1) + '°' + ' numero ');
+
+            // controlli input 
+            if (numeroUtente == null) {
+                count = 5;
+                stopGame = true;
             } else {
-                alert('non puoi inserire due volte lo stesso numero... per favore inserisci un altro numero');
+                numeroUtente = parseInt(numeroUtente);
+                if (isNaN(numeroUtente)) {
+                    alert('puoi inserire solo  numeri e non puoi lasiare il campo vuoto')
+                } else if (!arrRisposte.includes(numeroUtente)) {
+                    arrRisposte.push(numeroUtente);
+                    count++;
+                } else {
+                    alert('non puoi inserire due volte lo stesso numero... per favore inserisci un altro numero');
+                }
             }
+            // fine controlli input 
         }
         var risultato = controllaValori(arr,arrRisposte);
-        if (risultato) {
+        if (risultato && !stopGame) {
             alert('ti sei ricordato tutti i numeri.... BRAVO');
-        } else {
+        } else if(!risultato && !stopGame) {
             alert('mi dispiace hai sbagliato')
+
+        } else {
+            setTimeout(function() {
+                document.getElementById('countdown').innerHTML = "grazie per aver giocato! <br> ricarica la  pagina per iniziare di nuovo il gioco...";
+            },300)
         }
     } else {
-        console.log(--secondi + 1);
+        document.getElementById('countdown').innerHTML = (--secondi + 1);
     }
 },1000);
 
 
 
 
+// FUNZIONI 
 
+// funzione per creare un array di numeri random
 function numRandUni(lunghezza,min,max) {
     var arr = [];
     var j = 0;
@@ -52,13 +71,7 @@ function numRandUni(lunghezza,min,max) {
     return arr;
 }
 
-
-
-
-
-
-
-
+// funzione per controllare se un array è uguale all'altro
 function controllaValori(array1,array2) {
     var numeriUguali = 0;
     for(var i = 0; i < array1.length; i++) {
