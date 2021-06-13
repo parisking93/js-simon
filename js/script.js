133dbb6ef5a69036cb8c88911960cc985c6c2ded
emmanuelparisi <emmanuel.p2606@gmail.com>
2021-06-13 11:26:45 +0200

    add button gioca

--- js/script.js
+++ js/script.js
@@ -3,54 +3,60 @@
 // Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
-// Un alert() espone 5 numeri generati casualmente.
-var arr = numRandUni(5,1,100);
-// Da li parte un timer di 30 secondi.
-alert('questi sono i 5 numeri da ricordare: ' + arr);
-var secondi = 3;
-var stopGame = false; 
-var timer = setInterval(function(){
-    if (!secondi) {
-        document.getElementById('countdown').innerHTML = "";
-        clearInterval(timer);
-        // Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
-        var count = 0;
-        var arrRisposte = [];
-        while(count < 5) {
-            var numeroUtente = prompt('dammi il ' + (count + 1) + '°' + ' numero ');
+document.getElementById('gioca').addEventListener('click', function(){
+    // pulisco countdown se non è la prima volta che gioca 
+    document.getElementById('countdown').innerHTML = "";
+    // Un alert() espone 5 numeri generati casualmente.
+    var arr = numRandUni(5,1,100);
+    // Da li parte un timer di 30 secondi.
+    alert('questi sono i 5 numeri da ricordare: ' + arr);
+    var secondi = 3;
+    var stopGame = false; 
+    var timer = setInterval(function(){
+        if (!secondi) {
+            document.getElementById('countdown').innerHTML = "";
+            clearInterval(timer);
+            // Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+            var count = 0;
+            var arrRisposte = [];
+            while(count < 5) {
+                var numeroUtente = prompt('dammi il ' + (count + 1) + '°' + ' numero ');
 
-            // controlli input 
-            if (numeroUtente == null) {
-                count = 5;
-                stopGame = true;
-            } else {
-                numeroUtente = parseInt(numeroUtente);
-                if (isNaN(numeroUtente)) {
-                    alert('puoi inserire solo  numeri e non puoi lasiare il campo vuoto')
-                } else if (!arrRisposte.includes(numeroUtente)) {
-                    arrRisposte.push(numeroUtente);
-                    count++;
+                // controlli input 
+                if (numeroUtente == null) {
+                    count = 5;
+                    stopGame = true;
                 } else {
-                    alert('non puoi inserire due volte lo stesso numero... per favore inserisci un altro numero');
+                    numeroUtente = parseInt(numeroUtente);
+                    if (isNaN(numeroUtente)) {
+                        alert('puoi inserire solo  numeri e non puoi lasiare il campo vuoto')
+                    } else if (!arrRisposte.includes(numeroUtente)) {
+                        arrRisposte.push(numeroUtente);
+                        count++;
+                    } else {
+                        alert('non puoi inserire due volte lo stesso numero... per favore inserisci un altro numero');
+                    }
                 }
+                // fine controlli input 
             }
-            // fine controlli input 
-        }
-        var risultato = controllaValori(arr,arrRisposte);
-        if (risultato && !stopGame) {
-            alert('ti sei ricordato tutti i numeri.... BRAVO');
-        } else if(!risultato && !stopGame) {
-            alert('mi dispiace hai sbagliato')
+            var risultato = controllaValori(arr,arrRisposte);
+            if (risultato && !stopGame) {
+                alert('ti sei ricordato tutti i numeri.... BRAVO');
+            } else if(!risultato && !stopGame) {
+                alert('mi dispiace hai sbagliato')
 
+            } else {
+                setTimeout(function() {
+                    document.getElementById('countdown').innerHTML = "grazie per aver giocato!";
+                },300)
+            }
         } else {
-            setTimeout(function() {
-                document.getElementById('countdown').innerHTML = "grazie per aver giocato! <br> ricarica la  pagina per iniziare di nuovo il gioco...";
-            },300)
+            document.getElementById('countdown').innerHTML = (--secondi + 1);
         }
-    } else {
-        document.getElementById('countdown').innerHTML = (--secondi + 1);
-    }
-},1000);
+    },1000);
+
+});
+
 
 
 
